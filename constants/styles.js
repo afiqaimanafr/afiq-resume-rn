8bc3f99f2d924407b728fc55e79db0cae46c4c09
afiqaimanafr <aaafr98@gmail.com>
2022-05-25 12:05:35 +0800

    update: latest

--- constants/styles.js
+++ constants/styles.js
@@ -1,17 +1,9 @@
 export const GlobalStyles = {
   colors: {
-    midnightblue: "#191970",
-    powderblue: "#b0e0e6",
-    primary100: "#c6affc",
-    primary200: "#a281f0",
-    primary400: "#5721d4",
-    primary500: "#3e04c3",
-    primary700: "#2d0689",
-    primary800: "#200364",
-    accent500: "#f7bc0c",
-    error50: "#fcc4e4",
-    error500: "#9b095c",
-    gray500: "#39324a",
-    gray700: "#221c30",
+    midnightBlue: "#191970",
+    navy: "#000080",
+    lightblue: "#add8e6",
+    powderBlue: "#b0e0e6",
+    lightCyan: "#e0ffff",
   },
 };
