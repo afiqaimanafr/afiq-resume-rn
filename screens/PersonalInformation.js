cd808c60d36de56a8d24e0a651d6288b966a4e2d
afiqaimanafr <aaafr98@gmail.com>
2022-05-25 10:14:56 +0800

    update: latest

---
+++ screens/PersonalInformation.js
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function PersonalInformation() {
+  return (
+    <View>
+      <Text>Personal Information Page!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
