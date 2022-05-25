173a4bb7cb5814834d1d9cd17f1f4312c8fa193d
afiqaimanafr <aaafr98@gmail.com>
2022-05-25 10:41:53 +0800

    update: latest

--- App.js
+++ App.js
@@ -9,19 +9,22 @@ import Education from "./screens/Education";
 
 const Tab = createMaterialTopTabNavigator();
 const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen
-            name="Personal Information"
-            component={PersonalInformation}
-          />
-          <Tab.Screen name="Experience" component={Experience} />
-          <Tab.Screen name="Education" component={Education} />
-        </Tab.Navigator>
+        <Stack.Navigator>
+          <Tab.Navigator>
+            <Tab.Screen
+              name="Personal Information"
+              component={PersonalInformation}
+            />
+            <Tab.Screen name="Experience" component={Experience} />
+            <Tab.Screen name="Education" component={Education} />
+          </Tab.Navigator>
+        </Stack.Navigator>
       </NavigationContainer>
     </>
   );
