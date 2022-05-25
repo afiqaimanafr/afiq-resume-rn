25baa3aef4f82eab855f50be5262d6e7a5cb03b0
afiqaimanafr <aaafr98@gmail.com>
2022-05-25 11:23:36 +0800

    update: latest

--- App.js
+++ App.js
@@ -1,34 +1,36 @@
+import { StyleSheet, Platform } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import PersonalInformation from "./screens/PersonalInformation";
 import Experience from "./screens/Experience";
 import Education from "./screens/Education";
 
+const isAndroid = Platform.OS === "android";
 const Tab = createMaterialTopTabNavigator();
-const Stack = createNativeStackNavigator();
-
-function AfiqAiman() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Personal Information" component={PersonalInformation} />
-      <Tab.Screen name="Experience" component={Experience} />
-      <Tab.Screen name="Education" component={Education} />
-    </Tab.Navigator>
-  );
-}
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Afiq Aiman" component={AfiqAiman} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <SafeAreaProvider style={styles.screen}>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen
+              name="Personal Information"
+              component={PersonalInformation}
+            />
+            <Tab.Screen name="Experience" component={Experience} />
+            <Tab.Screen name="Education" component={Education} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: { flex: 1, marginTop: isAndroid ? 20 : 0 },
+});
