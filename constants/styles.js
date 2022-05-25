335cfc18e72336ffb067a3635428e29a111abc3e
afiqaimanafr <aaafr98@gmail.com>
2022-05-25 12:21:32 +0800

    update: latest

--- constants/styles.js
+++ constants/styles.js
@@ -2,8 +2,13 @@ export const GlobalStyles = {
   colors: {
     midnightBlue: "#191970",
     navy: "#000080",
+    steelBlue: "#4682b4",
     slateBlue: "#6a5acd",
     dodgerBlue: "#1e90ff",
+    teal: "#008080",
+    turquoise: "#40e0d0",
+    paleturquoise: "#afeeee",
+    skyblue: "#87ceeb",
     lightBlue: "#add8e6",
     powderBlue: "#b0e0e6",
     lightCyan: "#e0ffff",
